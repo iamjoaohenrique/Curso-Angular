18cb94d53512ae1fbeb730e8cbdd4b955e5ec4cc
Silva <johsilva@indracompany.com>
2020-02-13 13:52:01 -0300

    criação de componente detalhe

---
+++ src/app/usuario/usuario-detalhe/usuario-detalhe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario-detalhe',
+  templateUrl: './usuario-detalhe.component.html',
+  styleUrls: ['./usuario-detalhe.component.css']
+})
+export class UsuarioDetalheComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/usuario/usuario.module.ts
+++ src/app/usuario/usuario.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsuarioFormComponent } from './usuario-form/usuario-form.component';
+import { UsuarioDetalheComponent } from './usuario-detalhe/usuario-detalhe.component';
 
 
 
 
 @NgModule({
   declarations: [
-    UsuarioFormComponent
+    UsuarioFormComponent,
+    UsuarioDetalheComponent
   ],
   imports: [
     CommonModule
